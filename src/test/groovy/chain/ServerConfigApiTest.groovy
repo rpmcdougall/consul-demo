412aed1777fb4909c5e09079fdf2172055105dfd
Ryan McDougall <rp.mcdougall@gmail.com>
2018-12-04 23:20:21 -0600

    added create test

--- src/test/groovy/chain/ServerConfigApiTest.groovy
+++ src/test/groovy/chain/ServerConfigApiTest.groovy
@@ -3,13 +3,17 @@ package chain
 import com.ecwid.consul.v1.ConsulClient
 import com.pszymczyk.consul.ConsulProcess
 import com.pszymczyk.consul.ConsulStarterBuilder
+import groovy.json.JsonBuilder
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
+import org.omg.CORBA.portable.Delegate
 import ratpack.groovy.test.GroovyRatpackMainApplicationUnderTest
 import ratpack.test.ApplicationUnderTest
 import service.ConsulClientService
 import spock.lang.Shared
 import spock.lang.Specification
+import ratpack.test.http.TestHttpClient
+import ratpack.http.client.RequestSpec
 
 class ServerConfigApiTest extends Specification {
 
@@ -19,6 +23,10 @@ class ServerConfigApiTest extends Specification {
 
     @Shared
     ApplicationUnderTest aut = new GroovyRatpackMainApplicationUnderTest()
+
+
+    TestHttpClient client = aut.httpClient
+
 
 
     def setup() {
@@ -38,10 +46,31 @@ class ServerConfigApiTest extends Specification {
         consulClient.setKVValue("config/server/test.testing.com", JsonOutput.toJson([fqdn:"test.testing.com",ports:[514,8080] , role:"test", ip :"10.12.1.2"]))
 
         when:
-        def result = new JsonSlurper().parseText(aut.httpClient.get("/v1/config/server").body.text)
+        def result = new JsonSlurper().parseText(client.get("/v1/config/server").body.text)
         then:
 
         assert result.fqdn == "test.testing.com"
+    }
+
+    def "will create kv pair"() {
+
+        given:
+        Map obj = [fqdn:"test-create.testing.com",ports:[514,8080] , role:"test", ip :"10.12.1.2"]
+        def req = new JsonBuilder(obj).toPrettyString()
+
+        when:
+
+        client.requestSpec {
+            it.body.text req
+            it.body.type"application/json"
+        }
+
+        def res = new JsonSlurper().parseText(client.post("/v1/config/server").body.text)
+
+
+        then:
+
+        assert res.fqdn == "test-create.testing.com"
     }
 
 
